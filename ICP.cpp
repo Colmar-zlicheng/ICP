76e22b96f3347eef65a910849ebeadb1c6371327
colmar <zlicheng@sjtu.edu.cn>
2022-12-24 00:07:24 +0800

    read data

--- ICP.cpp
+++ ICP.cpp
@@ -13,22 +13,13 @@ int main()
     data_before = "data/points_before.pcd";
     data_after = "data/points_after.pcd";
 
+    PointXYZ center_before;
+    PointXYZ center_after;
     vector<PointXYZ> points_before;
     vector<PointXYZ> points_after;
     // read points from pcd file and pretreat data
-    readPCD(points_before, data_before, 11);
-    readPCD(points_after, data_after, 13);
-
-    // double center_before[3] = {0}, center_after[3] = {0};
-    // double *x = Centroid(data_before, data_after);
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     center_before[i] = x[i];
-    //     center_after[i] = x[i + 3];
-    // }
-    // delete[] x;
-    // cout << center_before[0] << " " << center_before[1] << " " << center_before[2] << endl;
-    // cout << center_after[0] << " " << center_after[1] << " " << center_after[2] << endl;
+    center_before = readPCD(points_before, data_before, 11);
+    center_after = readPCD(points_after, data_after, 13);
 
     // compute running time
     clock_t end_time = clock();
--- dataloader.h
+++ dataloader.h
@@ -9,43 +9,14 @@ struct PointXYZ
     double x, y, z;
 };
 
-// center[6] = before[0], before[1], before[2], after[0], after[1], after[2]
-double *save_center(double x[])
-{
-    double *center = new double[6];
-    center[0] = x[0];
-    center[1] = x[1];
-    center[2] = x[2];
-    center[3] = x[3];
-    center[4] = x[4];
-    center[5] = x[5];
-    return center;
-}
-
-double *Centroid(string data_before, string data_after)
-{
-    double *out;
-    double center[6] = {0};
-    center[0] = 0.111;
-    center[1] = 0.222;
-    center[2] = 0.333;
-    center[3] = 0.444;
-    center[4] = 0.555;
-    center[5] = 0.666;
-
-    out = save_center(center);
-    return out;
-}
-
 // Read 3D points in .pcd file
-
-void readPCD(vector<PointXYZ> &cloud, const string filename, const int header_lines)
+PointXYZ readPCD(vector<PointXYZ> &cloud, const string filename, const int header_lines)
 {
     ifstream file(filename);
     if (file.bad())
     {
         cout << "Failed to open file: " << filename << endl;
-        return;
+        exit(1);
     }
 
     char Header[header_lines][1024]; // Save header in pcd file(first 11 lines)
@@ -94,6 +65,8 @@ void readPCD(vector<PointXYZ> &cloud, const string filename, const int header_li
     cout << "Reading points of " << filename << endl;
     double tmp;
     PointXYZ p;
+    PointXYZ center;
+    center.x = center.y = center.z = 0;
 
     if ((data_columns_type == "x y z") && (data_type == "ascii"))
     {
@@ -103,6 +76,9 @@ void readPCD(vector<PointXYZ> &cloud, const string filename, const int header_li
         {
             file >> p.x >> p.y >> p.z >> tmp;
             i++;
+            center.x += p.x;
+            center.y += p.y;
+            center.z += p.z;
             // if (file.peek() == EOF)
             // {
             //     break;
@@ -111,4 +87,37 @@ void readPCD(vector<PointXYZ> &cloud, const string filename, const int header_li
         }
         cout << endl;
     }
+    center.x = center.x / double(Points_num);
+    center.y = center.y / double(Points_num);
+    center.z = center.z / double(Points_num);
+
+    return center;
 }
+
+// double *save_center(double x[])
+// {
+//     // center[6] = before[0], before[1], before[2], after[0], after[1], after[2]
+//     double *center = new double[6];
+//     center[0] = x[0];
+//     center[1] = x[1];
+//     center[2] = x[2];
+//     center[3] = x[3];
+//     center[4] = x[4];
+//     center[5] = x[5];
+//     return center;
+// }
+
+// double *Centroid(string data_before, string data_after)
+// {
+//     double *out;
+//     double center[6] = {0};
+//     center[0] = 0.111;
+//     center[1] = 0.222;
+//     center[2] = 0.333;
+//     center[3] = 0.444;
+//     center[4] = 0.555;
+//     center[5] = 0.666;
+
+//     out = save_center(center);
+//     return out;
+// }
